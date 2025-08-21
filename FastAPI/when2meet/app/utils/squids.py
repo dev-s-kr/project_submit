cc0eb504a32b3bc329a5afa7ac8382c514f19aa7
DESKTOP-NCSJNG2\Me <zkzktldk1234@gmail.com>
2025-08-21 13:22:15 +0900

    ✨ Install tortoise

--- FastAPI/when2meet/app/utils/squids.py
+++ FastAPI/when2meet/app/utils/squids.py
@@ -3,7 +3,6 @@ from typing import Sequence
 
 from sqids import sqids
 
-
 squid = sqids.Sqids()
 
 
@@ -13,8 +12,7 @@ class Squids:
     def encode(cls, nums: Sequence[int]) -> str:
         return squid.encode(nums)
 
+
 def do_squids():
     now = datetime.now()
-    return Squids.encode(
-        [now.year, now.month, now.day, now.hour, now.minute, now.second, now.microsecond]
-    )
+    return Squids.encode([now.year, now.month, now.day, now.hour, now.minute, now.second, now.microsecond])
